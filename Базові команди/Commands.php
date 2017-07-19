a85352e7da618560d704b7b3bdeff16c1912e060
fel kost <felkost@ukr.net>
2017-07-19 18:29:31 +0300

    Уточнено способи оформлення для виведення тексту

--- Базові команди/Commands.php
+++ Базові команди/Commands.php
@@ -1,6 +1,7 @@
 <?php
 //завдання : http://htmllab.ru/ustanovka-i-nastroyka-php-zadachi/
-echo "привіт \n"; //подвійні кавички, тоді працює \n, з одинарними - ні
+echo "привіт \n"; //в консольному режимі: подвійні кавички, тоді працює \n, з одинарними - ні;
+//в браузері застосовувати лише <br />
 echo 'Текущая версия PHP: ' . phpversion()."\n";  //http://php.net/manual/ru/function.phpversion.php
 echo 'Текущая дата: ' .date('d-m-Y H:i:s');
 // про теги та тег-закривання: http://php.net/manual/ru/language.basic-syntax.phptags.php
