fb09e479fe4f85fa4a944a879ebb4bdadfdd7547
fel kost <felkost@ukr.net>
2017-07-19 18:10:45 +0300

    Команди для перевірки версії, дати, доступності кирилиці та закриваючий тег

---
+++ Базові команди/Commands.php
@@ -0,0 +1,6 @@
+<?php
+//завдання : http://htmllab.ru/ustanovka-i-nastroyka-php-zadachi/
+echo "привіт \n"; //подвійні кавички, тоді працює \n, з одинарними - ні
+echo 'Текущая версия PHP: ' . phpversion()."\n";  //http://php.net/manual/ru/function.phpversion.php
+echo 'Текущая дата: ' .date('d-m-Y H:i:s');
+// про теги та тег-закривання: http://php.net/manual/ru/language.basic-syntax.phptags.php
